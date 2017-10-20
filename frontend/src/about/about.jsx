e9f848cc419c4dd5f8adb79e78f5305ef8e75862
Marcela Bomfim <marcela.bomfim@lojaskd.com.br>
2017-10-20 17:47:33 -0200

    new template component for page header

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="Sobre" small="Nós" />
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum...</p>
+
+    <h2>Missão e Visão</h2>
+    <p>Lorem ipsum...</p>
+
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum...</p>
   </div>
 )
