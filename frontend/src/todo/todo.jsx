07953946d3499e9a68adae33d415a1a9bfcfdd2d
Marcela Bomfim <marcela.bomfim@lojaskd.com.br>
2017-10-24 15:31:14 -0200

    implementing todo actions to remove or mark as done/pending

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,6 +14,10 @@ export default class Todo extends Component {
 
     this.handleChange = this.handleChange.bind(this)
     this.handleAdd = this.handleAdd.bind(this)
+    
+    this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+    this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
 
     this.refresh()
   }
@@ -32,6 +36,21 @@ export default class Todo extends Component {
     axios.post(URL, { description })
       .then(resp => this.refresh())
   }
+
+  handleMarkAsDone(todo) {
+    axios.put(`${URL}/${todo._id}`, {...todo, done: true })
+      .then(resp => this.refresh())
+  }
+
+  handleMarkAsPending(todo) {
+    axios.put(`${URL}/${todo._id}`, {...todo, done: false })
+    .then(resp => this.refresh())
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${URL}/${todo._id}`)
+      .then(resp => this.refresh())
+  }
   
   render() {
     return (
@@ -41,7 +60,12 @@ export default class Todo extends Component {
           description={this.state.description}
           handleChange={this.handleChange}
           handleAdd={this.handleAdd}/>
-        <TodoList list={this.state.list}/>
+        <TodoList 
+          list={this.state.list}
+          handleMarkAsDone={this.handleMarkAsDone}
+          handleMarkAsPending={this.handleMarkAsPending}
+          handleRemove={this.handleRemove}
+          />
       </div>
     )
   }
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
 export default props => {
   const renderRows = () => {
     const list = props.list || []
     return list.map(todo => 
       <tr key={todo._id}>
-        <td>{todo.description}</td>
+        <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+        <td>
+          <IconButton style="success" icon="check" hide={todo.done}
+            onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+          <IconButton style="warning" icon="undo" hide={!todo.done}
+            onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+          <IconButton style="danger" icon="trash-o" hide={!todo.done}
+            onClick={() => props.handleRemove(todo)}></IconButton>
+        </td>
       </tr>
     )
   }
@@ -15,6 +24,7 @@ export default props => {
        <thead>
          <tr>
            <th>Descrição</th>
+           <th>Ações</th>
          </tr>
        </thead>
        <tbody>
