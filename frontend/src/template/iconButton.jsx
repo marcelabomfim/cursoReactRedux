80e0184cd5d1a666b739e51fe2d1374d5cb0e215
Marcela Bomfim <marcela.bomfim@lojaskd.com.br>
2017-10-24 13:36:42 -0200

    improvement no iconButton component and create helper to test hide props

--- frontend/src/template/iconButton.jsx
+++ frontend/src/template/iconButton.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
+import If from './if'
 
-export default props => {
-  if (props.hide) {
-    return null
-  } else {
-    const styles = props.style.split(' ')
-    let styleClasses = 'btn'
-
-    styleClasses += styles.map((style) => ' btn-' + style).join('')
-
-    return (
-      <button className={classes} onClick={props.onClick}>
-        <i className={'fa fa-' + props.icon}></i>
-      </button>
-    )
-  }
-}
+export default props => (
+  <If test={!props.hide}>
+    <button className={
+        'btn' + 
+        props.style.split(' ').map((style) => ' btn-' + style).join('')
+      } onClick={props.onClick}>
+      <i className={'fa fa-' + props.icon}></i>
+    </button>
+  </If>
+)
