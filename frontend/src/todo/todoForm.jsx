5b3521a4ed91be8754dd6ee9b5421dc84ecff5c5
Marcela Bomfim <marcela.bomfim@lojaskd.com.br>
2017-10-24 13:50:31 -0200

    create functions to add and change events and controlling input value

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,10 +5,14 @@ import IconButton from '../template/iconButton'
 export default props => (
   <div role="form" className="todoForm">
     <Grid cols="12 9 10">
-      <input id="description" className="form-control" placeholder="Adiciona uma tarefa"/>
+      <input id="description" className="form-control" 
+        placeholder="Adiciona uma tarefa"
+        value={props.description}
+        onChange={props.handleChange}/>
     </Grid>
     <Grid cols="12 3 2">
-      <IconButton style="primary block" icon="plus"></IconButton>
+      <IconButton style="primary block" icon="plus" 
+        onClick={props.handleAdd}></IconButton>
     </Grid>
   </div>
 )
