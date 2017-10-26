eb73cf13624b7d3e1c40d89ebe463cd4caca3de8
Marcela Bomfim <marcela.bomfim@lojaskd.com.br>
2017-10-26 15:06:04 -0200

    implementing actions of mark as done/pending and clear via redux

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { changeDescription, search, add } from './todoActions'
+import { changeDescription, search, add, clear } from './todoActions'
 
 class TodoForm extends Component {
   constructor(props) {
@@ -17,11 +17,11 @@ class TodoForm extends Component {
   }
 
   keyHandler(e) {
-    const {add, search, description } = this.props
+    const {add, clear, search, description } = this.props
     if (e.key === 'Enter') {
-      e.shiftKey ? search : add(description)
+      e.shiftKey ? search() : add(description)
     } else if (e.key === 'Escape') {
-      this.props.handleClear()
+      clear()
     }
   }
 
@@ -42,7 +42,7 @@ class TodoForm extends Component {
           <IconButton style="info" icon="search" 
             onClick={() => search()}></IconButton>
           <IconButton style="default" icon="close" 
-            onClick={this.props.handleClear}></IconButton>
+            onClick={this.props.clear}></IconButton>
         </Grid>
       </div>
     )
@@ -56,7 +56,8 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => bindActionCreators({
   changeDescription,
   search,
-  add
+  add,
+  clear
 }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
