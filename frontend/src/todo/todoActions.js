1346ab30311fec024c7d887934b7d59f1e176177
Marcela Bomfim <marcela.bomfim@lojaskd.com.br>
2017-10-26 14:48:44 -0200

    implementing add action with midleware help

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -8,9 +8,22 @@ export const changeDescription = e => ({
 })
 
 export const search = () => {
-  const request = axios.get(`${URL}?sort=createAt`)
+  const request = axios.get(`${URL}?sort=createdAt`)
   return {
     type: 'TODO_SEARCHED',
     payload: request
+  }
+}
+
+export const add = (description) => {
+  return dispatch => {
+    axios.post(URL, { description })
+      .then(resp => dispatch({
+        type: 'TODO_ADDED',
+        payload: resp.data
+      }))
+      .then(resp => dispatch(
+        search()
+      ))
   }
 }
