b0164e4e618d8c8b65994e5e3df76b86fdfdfe7d
Marcela Bomfim <marcela.bomfim@lojaskd.com.br>
2017-10-26 15:14:04 -0200

    fixing search description

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -8,10 +8,11 @@ export const changeDescription = e => ({
 })
 
 export const search = () => {
-  const request = axios.get(`${URL}?sort=createdAt`)
-  return {
-    type: 'TODO_SEARCHED',
-    payload: request
+  return (dispatch, getState) => {
+    const description = getState().todo.description
+    const search = description ? `&description__regex=/${description}/` : ''
+    const request = axios.get(`${URL}?sort=-createdAt${search}`)
+      .then(resp => dispatch({ type: 'TODO_SEARCHED', payload: resp.data }))
   }
 }
 
